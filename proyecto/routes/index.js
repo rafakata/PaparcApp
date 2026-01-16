38f3e01879e32e8c44b7394ea17d973383041929
jcalduin <cabreramirandajavier@gmail.com>
2026-01-16 12:22:48 +0100

    cambio de rutas

--- proyecto/routes/index.js
+++ proyecto/routes/index.js
@@ -19,25 +19,6 @@ router.get('/privacy', function(req, res, next) {
   res.render('privacy', { title: 'Política de Privacidad' });
 });
 
-/* !! Comentario de Satori: !!
-He añadido nuevas rutas para las páginas de dashboard, register y login que se se habían borrado
-*/
-
-/* GET dashboard page */
-/*router.get('/dashboard', function(req, res, next) {
-  res.render('dashboard', { title: 'Dashboard' });
-});*/
-
-/* GET register page */
-/*router.get('/register', function(req, res, next) {
-  res.render('register', { title: 'Register' });
-});*/
-
-/* GET login page */
-/*router.get('/login', function(req, res, next) {
-  res.render('login', { title: 'Login' });
-});*/
-
 
 
 module.exports = router;
